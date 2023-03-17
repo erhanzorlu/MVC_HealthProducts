d68b24391053d91e52cf336aafba840d9572fe07
erhanzorlu <zorlu.com@gmail.com>
2023-03-17 17:10:51 +0300

    .

---
+++ Project.DAL/ContextClasses/MyContext.cs
@@ -0,0 +1,28 @@
+﻿using Project.ENTITIES.Models;
+using Project.MAP.Options;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.ContextClasses
+{
+    public  class MyContext:DbContext
+    {
+        public MyContext():base("MyConnection")
+        {
+            
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new ProductMap());
+
+
+        }
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+}
---
+++ Project.MAP/Options/BaseMap.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.MAP.Options
+{
+    public class BaseMap<T>:EntityTypeConfiguration<T> where T : class
+    {
+    }
+}
