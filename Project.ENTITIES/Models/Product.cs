89c47ede582a9a65fd5947d3672860759025e30c
erhanzorlu <zorlu.com@gmail.com>
2023-03-17 16:21:01 +0300

    .

---
+++ Project.ENTITIES/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.ENTITIES.Models
+{
+    public class Product:BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
