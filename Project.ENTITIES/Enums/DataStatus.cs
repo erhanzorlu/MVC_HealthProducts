e14363ee8abbf33349a58f97685bd904bbb2abdc
erhanzorlu <zorlu.com@gmail.com>
2023-03-17 13:09:06 +0300

    Add project files.

---
+++ Project.ENTITIES/Enums/DataStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.ENTITIES.Enums
+{
+   public enum DataStatus
+    {
+        Inserted=1,
+        Updated=2,
+        Deleted=3,
+        
+
+    }
+}
