6d7f7079984567b7e14e00603b2247c76c18afad
Boran Kaya <borankaya210@gmail.com>
2023-03-26 00:12:57 +0300

    Map Katmanı Tamamlandı.

---
+++ Project.MAP/Options/CategoryMap.cs
@@ -0,0 +1,19 @@
+﻿using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.MAP.Options
+{
+    public class CategoryMap  : BaseMap<Category>
+    {
+        public CategoryMap()
+        {
+            
+        }
+
+    }
+}
