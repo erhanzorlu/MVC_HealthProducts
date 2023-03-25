f6b13deaa8f3c1b2ae2bf69ffa2c83a7b965a2e9
altankodalak <altankodalak@outlook.com>
2023-03-25 23:53:03 +0300

    BLL Katmanı tamamlandı

---
+++ Project.BLL/GenericRepository/BaseRep/BaseRepository.cs
@@ -0,0 +1,129 @@
+﻿using Project.BLL.DesingPaterns.SingletonPatern;
+using Project.BLL.GenericRepository.IntRep;
+using Project.DAL.ContextClasses;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.GenericRepository.BaseRep
+{
+    public abstract class BaseRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        protected MyContext _db;
+        public BaseRepository()
+        {
+            _db = DBTool.DbInstance;
+        }
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(List<T> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Any(Expression<Func<T, bool>> exp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteRange(List<T> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Destroy(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DestroyRange(List<T> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Find(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FirstOrDefault(Expression<Func<T, bool>> exp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetActives()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetCountedDatas(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetFirstDatas(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetLastDatas(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetModifieds()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetPassives()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<X> Select<X>(Expression<Func<T, X>> exp) where X : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateRange(List<T> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> Where(Expression<Func<T, bool>> exp)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void Save()
+        {
+            _db.SaveChanges();
+        }
+
+     
+    }
+}
---
+++ Project.BLL/GenericRepository/IntRep/IRepository.cs
@@ -0,0 +1,54 @@
+﻿using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.GenericRepository.IntRep
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        //List Commands
+
+        List<T> GetAll();
+        List<T> GetActives();
+        List<T> GetPassives();
+        List<T> GetModifieds();
+
+        //Modify Commands
+
+        void Add(T item);
+        void AddRange(List<T> list);
+        void Update(T item);
+        void UpdateRange(List<T> list);
+        void Delete(T item);
+        void DeleteRange(List<T> list);
+        void Destroy(T item);
+        void DestroyRange(List<T> list);
+
+        //Linq Commands
+
+        List<T> Where(Expression<Func<T, bool>> exp);
+
+        bool Any(Expression<Func<T, bool>> exp);
+
+        T FirstOrDefault(Expression<Func<T, bool>> exp);
+
+        IQueryable<X> Select<X>(Expression<Func<T, X>> exp) where X : class;
+
+        //Find Command
+        T Find(int id);
+
+        //get last datas
+        List<T> GetLastDatas(int number);
+
+        //get first datas
+        List<T> GetFirstDatas(int number);
+
+        //get counted datas
+        List<T> GetCountedDatas(int number);
+
+    }
+}
